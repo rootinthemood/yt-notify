1af06bb659a3932e20d90146911aa6799b709842
rootinthemood <>
2022-04-05 11:59:37 +0200

    Moved init to main.py

--- main.py
+++ main.py
@@ -5,27 +5,21 @@ from functions import add_channel
 CHANNELS = dict()
 CHANNEL_JSON = "test.json"
 
-def init():
-    global CHANNELS
-    global CHANNEL_JSON
-    if not os.path.isfile(CHANNEL_JSON):
-        with open(CHANNEL_JSON, 'w') as f:
-            to_write = {'channels': []}
-            json.dump(to_write, f)
+if not os.path.isfile(CHANNEL_JSON):
+    with open(CHANNEL_JSON, 'w') as f:
+        to_write = {'channels': []}
+        json.dump(to_write, f)
 
-    with open(CHANNEL_JSON, 'r') as f:
-        CHANNELS = json.load(f)
+with open(CHANNEL_JSON, 'r') as f:
+    CHANNELS = json.load(f)
 
 def write_json():
     with open (CHANNEL_JSON, 'w') as f:
         json.dump(CHANNELS, f)
 
 
-init()
-
-
-#add_channel("NileBlue", "http://youtube.com", CHANNELS)
-#add_channel("V for Valentine", "http://youtube.com", CHANNELS)
+add_channel("NileBlue", "http://youtube.com", CHANNELS)
+add_channel("V for Valentine", "http://youtube.com", CHANNELS)
 
 write_json()
 
