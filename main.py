27b61b75a5fb50f5b208d91b5156a44301b4e3a5
Unknown <unknown@localhost.localdomain>
2022-04-06 13:51:41 +0200

    Implemented update_channel fuction. This Scrapes the last 'n' videos from a channel and compares them to the already saved videos.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 import os
 import os
 from functions import add_channel, return_id_title_seen, set_channel_seen, print_id_title_seen
-from scrapevideos import scrape_channel, scrape_all_channels
+from scrapevideos import scrape_channel, scrape_all_channels ,update_channel
 
 CHANNELS = dict()
 CHANNEL_JSON = "test.json"
@@ -24,9 +24,14 @@ def write_json():
 #add_channel("V for Valentine", "https://www.youtube.com/c/VforValentine/", CHANNELS)
 #scrape_all_channels(CHANNELS)
         
+temp_videos = update_channel("NileBlue", CHANNELS)
+print(CHANNELS['NileBlue'])
+print(len(CHANNELS['NileBlue']))
+#print(CHANNELS['NileBlue'][0])
+
 
 #set_channel_seen("NileBlue", CHANNELS, False)
-print_id_title_seen("NileBlue", CHANNELS)
+#print_id_title_seen("NileBlue", CHANNELS)
 #write_json()
 
 
