091b4fe902bc8b06cbf3deda23f4d55d6f8dd7b5
rootinthemood <>
2022-04-06 13:51:41 +0200

    Implemented update_channel fuction. This Scrapes the last 'n' videos from a channel and compares them to the already saved videos.

--- scrapevideos.py
+++ scrapevideos.py
@@ -20,3 +20,28 @@ def scrape_all_channels(channel_list):
             video_id = video['videoId']
             title = video['title']['runs'][0]['text']
             channel_list[channel['name']].append({'video_id': video_id, 'title': title, 'seen': ""})
+
+def update_channel(channel_name, channel_list):
+    """Scrapes the last 'n' videos from a channel and compares them to the already saved videos"""
+    temp_videos = []
+    for channel in channel_list['channels']:
+        if channel_name == channel['name']:
+            videos = scrapetube.get_channel(channel_url=channel['url'], limit=10)
+            for video in videos:
+                video_id = video['videoId']
+                title = video['title']['runs'][0]['text']
+                temp_videos.append({'video_id': video_id, 'title': title, 'seen': False})
+
+    for old_video in channel_list[channel_name]:
+        for video in temp_videos:
+            if old_video['video_id'] == video['video_id']:
+                temp_videos.remove(video)
+    if len(temp_videos) <= 0:
+        print("No new videos found")
+        return
+    else:
+        count = 0
+        for video in temp_videos:
+            channel_list[channel_name].insert(0, video)
+            count += 1
+    print(f"{count} new video(s) found.")
