89e95aed589dc8e33db9f01832fec931a2ee3dd3
Unknown <unknown@localhost.localdomain>
2022-04-09 17:04:27 +0200

    Uncheck/check all buttons added in gui.py

--- gui.py
+++ gui.py
@@ -1,25 +1,96 @@
 import tkinter
-
-root = tkinter.Tk()
-root.title("yt-notify")
-root.geometry("400x900")
+import os
+from functions import write_json
 
 
-def get_size():
-    width = root.winfo_width()
-    height= root.winfo_height()
-    print(width, height)
-    root.after(1000, get_size)
+def all_videos_window(channel_list, channel_name, json_location):
+    """Makes a window for a given channel and lists all videotitles with a checkbutton, """
+    window_name = channel_name + "_window"
+    window_name = tkinter.Tk()
+    window_name.title(channel_name)
+    window_name.minsize(width=600, height=600)
+    window_name.maxsize(width=600, height=600)
+    window_name.config(padx=20, pady=20)
 
-root.after(1000, get_size)
+    def save():
+        """Saves the values from all checkboxes to the main channel"""
+        for key, value in var_list.items():
+            for index, video in enumerate(channel_list[channel_name]):
+                if key == video['video_id']: 
+                    if value.get() == 1:
+                        channel_list[channel_name][index]['seen'] = True
+                    elif value.get() == 0:
+                        channel_list[channel_name][index]['seen'] = False
+
+        write_json(channel_list, json_location)
+
+
+    def check_all():
+        for key, value in var_list.items():
+            value.set(1)
+
+    def uncheck_all():
+        for key, value in var_list.items():
+            value.set(0)
+
+    canvas = tkinter.Canvas(window_name)
+    scroll_y = tkinter.Scrollbar(window_name, orient='vertical', command=canvas.yview)
+    frame = tkinter.Frame(canvas)
+
+    frame_title = tkinter.Frame(window_name)
+    frame_title.pack()
+
+    #Make Title and Seen labels
+    label_title = tkinter.Label(frame_title, text="Title", font=('Ariel', 20, 'bold'))
+    label_title.grid(column=0, row=0, sticky="W")
+
+    label_blanc = tkinter.Label(frame_title, text="           \t\t\t\t       ")
+    label_blanc.grid(column=1, row=0)
+
+    label_seen = tkinter.Label(frame_title, text="Seen", font=('Ariel', 20, 'bold'))
+    label_seen.grid(column=2, row=0, sticky="E")
+
+    #Make Save and Print buttons
+    button_save = tkinter.Button(text="Save", command=save)
+    button_save.pack()
+
+    button_uncheck = tkinter.Button(text="Check all", command=check_all)
+    button_uncheck.pack()
+
+    button_check = tkinter.Button(text="uncheck all", command=uncheck_all)
+    button_check.pack()
+
+    #Make Label for title and checkbox for seen for every video
+    label_channels=[]
+    var_list={}
+
+    for index, channel in enumerate(channel_list[channel_name]):
+        #Make title shorter if too long
+        title = channel['title']
+        if len(title) > 57:
+            title = title[0:57]
+
+        #Make label for each title
+        label_channels.append(tkinter.Label(frame, text=title).grid(column=0, row=index+1, sticky="W"))
+
+        #Sets checkbox on/off for for video true/false
+        if channel['seen'] == True:
+            var_list[channel['video_id']]= tkinter.IntVar(frame, value=1)
+        else:
+            var_list[channel['video_id']] = tkinter.IntVar(frame, value=0)
+
+        #Make checkboxes
+        tkinter.Checkbutton(frame, variable=var_list[channel['video_id']]).grid(column=1, row=index+1)
+
+    #Creates a canvas in a frame so a scrollbar can be added
+    canvas.create_window(0, 0, anchor='nw', window=frame)
+    canvas.update_idletasks()
+    canvas.configure(scrollregion=canvas.bbox('all'),
+                     yscrollcommand=scroll_y.set)
+
+    canvas.pack(fill='both', expand=True, side='left')
+    scroll_y.pack(fill='y', side='right')
 
 
 
-
-
-
-
-
-
-
-root.mainloop()
+    window_name.mainloop()
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import tkinter
 from functions import add_channel, return_id_title_seen, set_channel_seen, print_id_title_seen, remove_channel, write_json
 from scrapevideos import scrape_channel, scrape_all_channels ,update_channel, update_all_channels
+from gui import all_videos_window
 
 CHANNELS = dict()
 CHANNEL_JSON = "test.json"
@@ -20,112 +21,118 @@ with open(CHANNEL_JSON, 'r') as f:
 #write_json(CHANNELS, CHANNEL_JSON)
 
 #####################--------GUI--------#########################
-root = tkinter.Tk()
-root.title("yt-notify")
-root.minsize(width=600, height=600)
-root.config(padx=20, pady=20)
+#root = tkinter.Tk()
+#root.title("yt-notify")
+#root.minsize(width=600, height=600)
+#root.config(padx=20, pady=20)
 
-def save():
-    for key, value in var_list.items():
-        for index, video in enumerate(CHANNELS['NileBlue']):
-            if key == video['video_id']: 
-                if value.get() == 1:
-                    CHANNELS['NileBlue'][index]['seen'] = True
-                elif value.get() == 0:
-                    CHANNELS['NileBlue'][index]['seen'] = False
-
-def print_channels():
-    os.system('clear')
-    print(CHANNELS)
-
-canvas = tkinter.Canvas(root)
-scroll_y = tkinter.Scrollbar(root, orient='vertical', command=canvas.yview)
-
-frame = tkinter.Frame(canvas)
-
-frame_title = tkinter.Frame(root)
-
-#Print titles
-label_title = tkinter.Label(frame_title, text="Title", font=('Ariel', 20, 'bold'))
-label_title.grid(column=0, row=0, sticky="W")
-
-label_blanc = tkinter.Label(frame_title, text="           \t\t\t\t       ")
-label_blanc.grid(column=1, row=0)
-
-label_seen = tkinter.Label(frame_title, text="Seen", font=('Ariel', 20, 'bold'))
-label_seen.grid(column=2, row=0, sticky="E")
-
-frame_title.pack()
-
-button_save = tkinter.Button(text="Save", command=save)
-button_save.pack()
-
-button_print = tkinter.Button(text="Print", command=print_channels)
-button_print.pack()
-
-
-#Print checkbox for each video
-label_channels=[]
-var_list={}
-
-for index, channel in enumerate(CHANNELS['NileBlue']):
-    label_channels.append(tkinter.Label(frame, text=channel['title']).grid(column=0, row=index+1, sticky="W"))
-    if channel['seen'] == True:
-        var_list[channel['video_id']]= tkinter.IntVar(frame, value=1)
-    else:
-        var_list[channel['video_id']] = tkinter.IntVar(frame, value=0)
-
-    tkinter.Checkbutton(frame, variable=var_list[channel['video_id']]).grid(column=1, row=index+1)
-
-canvas.create_window(0, 0, anchor='nw', window=frame)
-canvas.update_idletasks()
-canvas.configure(scrollregion=canvas.bbox('all'),
-                 yscrollcommand=scroll_y.set)
-
-canvas.pack(fill='both', expand=True, side='left')
-scroll_y.pack(fill='y', side='right')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-root.mainloop()
-
-#def write_json(channel_list, json_location):
-#    with open (CHANNEL_JSON, 'w') as f:
-#        json.dump(CHANNELS, f, indent=2)
-
-
-#add_channel(CHANNELS)
-#scrape_all_channels(CHANNELS)
-#remove_channel("V for Valentine", CHANNELS)        
-#print(CHANNELS)
-#update_all_channels(CHANNELS)
-#for channel in CHANNELS:
-#    if channel == "channels":
-#        continue
-#    update_channel(channel, CHANNELS)
-#update_channel("NileBlue", CHANNELS)
-#print(CHANNELS['NileBlue'])
-#print(len(CHANNELS['NileBlue']))
-#print(CHANNELS['NileBlue'][0])
-
-#scrape_all_channels(CHANNELS)
-#scrape_channel("V for Valentine", CHANNELS)
-#print(CHANNELS)
-#print(CHANNELS['V for Valentine'])
-#set_channel_seen("NileBlue", CHANNELS, True)
-#print_id_title_seen("NileBlue", CHANNELS)
+for channel in CHANNELS:
+    if channel == "channels":
+        continue
+    all_videos_window(CHANNELS, channel, CHANNEL_JSON)
+#all_videos_window(CHANNELS, "V for Valentine")
+#all_videos_window(CHANNELS, "NileBlue", "test2")
+#def save():
+#    for key, value in var_list.items():
+#        for index, video in enumerate(CHANNELS['NileBlue']):
+#            if key == video['video_id']: 
+#                if value.get() == 1:
+#                    CHANNELS['NileBlue'][index]['seen'] = True
+#                elif value.get() == 0:
+#                    CHANNELS['NileBlue'][index]['seen'] = False
+#
+#def print_channels():
+#    os.system('clear')
+#    print(CHANNELS)
+#
+#canvas = tkinter.Canvas(root)
+#scroll_y = tkinter.Scrollbar(root, orient='vertical', command=canvas.yview)
+#
+#frame = tkinter.Frame(canvas)
+#
+#frame_title = tkinter.Frame(root)
+#
+##Print titles
+#label_title = tkinter.Label(frame_title, text="Title", font=('Ariel', 20, 'bold'))
+#label_title.grid(column=0, row=0, sticky="W")
+#
+#label_blanc = tkinter.Label(frame_title, text="           \t\t\t\t       ")
+#label_blanc.grid(column=1, row=0)
+#
+#label_seen = tkinter.Label(frame_title, text="Seen", font=('Ariel', 20, 'bold'))
+#label_seen.grid(column=2, row=0, sticky="E")
+#
+#frame_title.pack()
+#
+#button_save = tkinter.Button(text="Save", command=save)
+#button_save.pack()
+#
+#button_print = tkinter.Button(text="Print", command=print_channels)
+#button_print.pack()
+#
+#
+##Print checkbox for each video
+#label_channels=[]
+#var_list={}
+#
+#for index, channel in enumerate(CHANNELS['NileBlue']):
+#    label_channels.append(tkinter.Label(frame, text=channel['title']).grid(column=0, row=index+1, sticky="W"))
+#    if channel['seen'] == True:
+#        var_list[channel['video_id']]= tkinter.IntVar(frame, value=1)
+#    else:
+#        var_list[channel['video_id']] = tkinter.IntVar(frame, value=0)
+#
+#    tkinter.Checkbutton(frame, variable=var_list[channel['video_id']]).grid(column=1, row=index+1)
+#
+#canvas.create_window(0, 0, anchor='nw', window=frame)
+#canvas.update_idletasks()
+#canvas.configure(scrollregion=canvas.bbox('all'),
+#                 yscrollcommand=scroll_y.set)
+#
+#canvas.pack(fill='both', expand=True, side='left')
+#scroll_y.pack(fill='y', side='right')
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#root.mainloop()
+#
+##def write_json(channel_list, json_location):
+##    with open (CHANNEL_JSON, 'w') as f:
+##        json.dump(CHANNELS, f, indent=2)
+#
+#
+##add_channel(CHANNELS)
+##scrape_all_channels(CHANNELS)
+##remove_channel("V for Valentine", CHANNELS)        
+##print(CHANNELS)
+##update_all_channels(CHANNELS)
+##for channel in CHANNELS:
+##    if channel == "channels":
+##        continue
+##    update_channel(channel, CHANNELS)
+##update_channel("NileBlue", CHANNELS)
+##print(CHANNELS['NileBlue'])
+##print(len(CHANNELS['NileBlue']))
+##print(CHANNELS['NileBlue'][0])
+#
+##scrape_all_channels(CHANNELS)
+##scrape_channel("V for Valentine", CHANNELS)
+##print(CHANNELS)
+##print(CHANNELS['V for Valentine'])
+##set_channel_seen("NileBlue", CHANNELS, True)
+##print_id_title_seen("NileBlue", CHANNELS)
 #write_json(CHANNELS, CHANNEL_JSON)
-
-
-#print(CHANNELS)
+#
+#
+##print(CHANNELS)
