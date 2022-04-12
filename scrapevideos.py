79dccb88c5abb5dad18a86d28abf641dddfd3ec7
rootinthemood <>
2022-04-12 14:00:18 +0200

    Reworked scrapevideos.py -> update_channels/update_all_channels. Added Update all channels to menu

--- scrapevideos.py
+++ scrapevideos.py
@@ -36,35 +36,46 @@ def scrape_all_channels(channel_list):
 def update_channel(channel_name, channel_list):
     """Scrapes the last 'n' videos from a channel and compares them to the already saved videos and then updates the main video dictionary"""
     #Scrapes the last 'n' videos from a given channel and appends them to a temp list
-    temp_videos = []
     for channel in channel_list['channels']:
         if channel_name == channel['name']:
-            videos = scrapetube.get_channel(channel_url=channel['url'], limit=10)
-            for video in videos:
-                video_id = video['videoId']
-                title = video['title']['runs'][0]['text']
-                temp_videos.append({'video_id': video_id, 'title': title, 'seen': False})
-    #Compares the temp list with the main 'CHANNELS' list and removes duplicates from temp list
-    for old_video in channel_list[channel_name]:
-        for video in temp_videos:
-            if old_video['video_id'] == video['video_id']:
-                temp_videos.remove(video)
-    #Prints found videos and inserts temp list(new videos) to main 'CHANNELS' dictionary
-    if len(temp_videos) <= 0:
-        return f"{channel_name}\t - No new videos found"
-#        print(f"{channel_name}\t - No new videos found")
-#        return
-    else:
-        count = 0
-        for video in temp_videos:
-            channel_list[channel_name].insert(0, video)
-            count += 1
-    return f"{channel_name}\t - {count} new video(s) found."
-#    print(f"{channel_name}\t - {count} new video(s) found.")
+            limit_count = 10
+            #Keeps scraping videos until the new videos found is less then limit count
+            while True:
+                temp_videos = []
+                print(limit_count)
+                videos = scrapetube.get_channel(channel_url=channel['url'], limit=limit_count)
+                for video in videos:
+                    video_id = video['videoId']
+                    title = video['title']['runs'][0]['text']
+                    temp_videos.append({'video_id': video_id, 'title': title, 'seen': False})
+                #Compares the temp list with the main 'CHANNELS' list and removes duplicates from temp list
+                for old_video in channel_list[channel_name]:
+                    for video in temp_videos:
+                        if old_video['video_id'] == video['video_id']:
+                            temp_videos.remove(video)
+                if len(temp_videos) == limit_count:
+                    limit_count += 20
+                    continue
+                break
+            #Prints found videos and inserts temp list(new videos) to main 'CHANNELS' dictionary
+            if len(temp_videos) <= 0:
+                return f"{channel_name}\t - No new videos found"
+        #        print(f"{channel_name}\t - No new videos found")
+        #        return
+            else:
+                count = 0
+                for video in reversed(temp_videos):
+                    channel_list[channel_name].insert(0, video)
+                    count += 1
+            return f"{channel_name}\t - {count} new video(s) found."
+        #    print(f"{channel_name}\t - {count} new video(s) found.")
 
 
 def update_all_channels(channel_list):
+    updates = []
     for channel in channel_list:
         if channel == "channels":
             continue
-        update_channel(channel, channel_list)
+        update = update_channel(channel, channel_list)
+        updates.append(update)
+    return updates
