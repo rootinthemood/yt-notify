b28cf316306bdb9991c92c3bb8249e0f34de5364
Unknown <unknown@localhost.localdomain>
2022-04-18 18:59:27 +0200

    Working video treewidget

--- videos_window.py
+++ videos_window.py
@@ -1,10 +1,13 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QPushButton, QLineEdit, QCheckBox, QTextEdit, QGridLayout, QMenu, QScrollArea, QVBoxLayout
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QPushButton, QLineEdit, QCheckBox, QTextEdit, QGridLayout, QMenu, QScrollArea, QVBoxLayout, QTreeWidgetItem
 from PyQt6.QtGui import QAction, QIcon, QFont
 from PyQt6.QtCore import Qt, QSize
+from PyQt6 import QtCore
 from functions import init_database
+from video_window_ui import Ui_Form
 
 
-class VideoWindow(QMainWindow):
+class VideoWindow(QWidget):
 
     def __init__(self, channel_list, channel_name):
         super().__init__()
@@ -13,56 +16,45 @@ class VideoWindow(QMainWindow):
         self.initializeUI()
 
     def initializeUI(self):
-        self.setMinimumSize(300, 172)
-        self.setMaximumSize(900, 780)
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
         self.setWindowTitle("yt-notify")
         self.setWindowIcon(QIcon("images/icon.ico"))
-
-        self.setUpMainWindow()
-
-    def setUpMainWindow(self):
-        self.scroll = QScrollArea()
-        self.widget = QWidget()
-        self.vbox = QVBoxLayout()
-#        self.main_grid = QGridLayout()
+        self.ui.label_title.setText(self.channel_name)
+        short_title = ""
         for index, channel in enumerate(self.channel_list[self.channel_name]):
             title = channel['title']
-            self.label = QLabel(title, self)
-            self.vbox.addWidget(self.label)
-        self.widget.setLayout(self.vbox)
-        self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
-        self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
-        self.scroll.setWidgetResizable(True)
-        self.scroll.setWidget(self.widget)
+            if len(title) > 57:
+                short_title = title[0:52] + "..."
+            elif len(title) <= 57:
+                short_title = title[0:len(title)] +  (57 - len(title)) * " "
+            else:
+                short_title = title
+            seen = str(channel['seen'])
+            item = QTreeWidgetItem(self.ui.treeWidget, [short_title])
+            item.setToolTip(0, title)
 
-        self.setCentralWidget(self.scroll)
+            if channel['seen'] == True:
+                item.setCheckState(1, QtCore.Qt.CheckState.Checked)
+            else:
+                item.setCheckState(1, QtCore.Qt.CheckState.Unchecked)
 
-        self.setGeometry(600, 100, 1000, 900)
-        self.setWindowTitle('Scroll Area Demonstration')
-        self.show()
+        self.ui.treeWidget.resizeColumnToContents(0)
+        self.resize(500, 729)
 
-        return
-        #scroll area properties
-#            self.main_grid.setRowStretch(1, 1)
-#            self.main_grid.addWidget(self.label, index, 0)
-#        self.setLayout(self.main_grid)
+        self.ui.button_close.clicked.connect(self.close)
 
-#        column_int = 0
-#        row_int = 0
-#        self.main_grid = QGridLayout()
-#
-#        #Set the central widget(needed because of QMainWindow)
-#
-#        for channel in self.channel_list['channels']:
-#            if channel == "channels":
-#                continue
-#            if column_int == 3:
-#                row_int += 1
-#                column_int = 0
-#            name = channel['name']
-#
-#            self.chan_button = QPushButton(name, self)
-#
-#            self.main_grid.addWidget(self.chan_button, row_int, column_int)
-#            column_int += 1
-#        self.setLayout(self.main_grid)
+    def checkAll(self):
+        pass
+
+    def uncheckAll(self):
+        pass
+
+    def save(self):
+        pass
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = VideoWindow()
+    main.show()
+    sys.exit(app.exec_())
