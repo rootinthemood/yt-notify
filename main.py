d35d4e1a65fe17b97804b24b41d3bf6b812e8204
rootinthemood <>
2022-11-11 13:25:50 +0100

    Update to 0.5.2

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ CHANNELS = init_database(CHANNEL_JSON)
 SETTINGS_LOCATION = os.path.abspath("./data/settings.ini")
 SETTINGS = init_settings(SETTINGS_LOCATION)
 
-VERSION = "0.5.1"
+VERSION = "0.5.2"
 
 class MainWindow(QMainWindow):
     def __init__(self):
